990c64735eb5bfab389c69fa0cf41df64a02e1c1
Henryk Modzelewski <henryk_modzelewski@mac.com>
2016-11-24 17:23:10 -0800

    fix for mvec joFFT

--- src/joLinearFunctionMiscConstructors.jl
+++ src/joLinearFunctionMiscConstructors.jl
@@ -4,28 +4,34 @@
 
 # FFT operators: joDFT
 function apply_fft(v,ms)
-    l=length(ms)
-    mp=prod(ms)
-    lv=length(v)
+    l::Integer=length(ms)
+    mp::Integer=prod(ms)
+    lv::Integer=length(v)
+    lvmp::Integer=lv/mp
     vs=collect(ms)
-    push!(vs,lv/mp)
+    if lvmp>1; push!(vs,lvmp); end
     rv=reshape(v,vs...)
-    return vec(fft(rv,1:l))
+    rv=fft(rv,1:l)
+    lvmp>1 ? rv=reshape(rv,mp,lvmp) : rv=vec(rv)
+    return rv
 end
 function apply_ifft(v,ms)
-    l=length(ms)
-    mp=prod(ms)
-    lv=length(v)
+    l::Integer=length(ms)
+    mp::Integer=prod(ms)
+    lv::Integer=length(v)
+    lvmp::Integer=lv/mp
     vs=collect(ms)
-    push!(vs,lv/mp)
+    if lvmp>1; push!(vs,lvmp); end
     rv=reshape(v,vs...)
-    return vec(ifft(rv,1:l))
+    rv=ifft(rv,1:l)
+    lvmp>1 ? rv=reshape(rv,mp,lvmp) : rv=vec(rv)
+    return rv
 end
 export joDFT
 """
 Multi-dimensional FFT transform over fast dimensions
 
-    joDFT(m,[n ...])
+    joDFT(m[,n[, ...]])
 
 Note: passes isadjoint(joDFT(m),m)
 """
