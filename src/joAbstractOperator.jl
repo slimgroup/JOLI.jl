2f0e69a5423317be9b7b573886fb90e2f6df107e
Henryk Modzelewski <henryk_modzelewski@mac.com>
2016-11-27 14:39:47 -0800

    updated joAbstractOperator.jl with new abstracts

--- src/joAbstractOperator.jl
+++ src/joAbstractOperator.jl
@@ -63,6 +63,15 @@ ctranspose(A::joAbstractOperator) = throw(joAbstractOperatorException("jo' not i
 # conj(jo)
 conj(A::joAbstractOperator) = throw(joAbstractOperatorException("conj(jo) not implemented"))
 
+# isreal(jo)
+isreal(A :: joAbstractOperator) = throw(joAbstractOperatorException("isreal(jo) not implemented"))
+
+# issymmetric(jo)
+issymmetric(A :: joAbstractOperator) = throw(joAbstractOperatorException("issymmetric(jo) not implemented"))
+
+# ishermitian(jo)
+ishermitian(A :: joAbstractOperator) = throw(joAbstractOperatorException("ishermitian(jo) not implemented"))
+
 ############################################################
 ## overloaded Base *(...jo...)
 
@@ -272,5 +281,17 @@ vcat(ops :: joAbstractOperator...)  = throw(joAbstractOperatorException("vcat(jo
 ############################################################
 ## extra methods
 
+# double(jo)
 double(A::joAbstractOperator)  = throw(joAbstractOperatorException("double(jo) not implemented"))
 
+# iscomplex(jo)
+iscomplex(A :: joAbstractOperator) = throw(joAbstractOperatorException("iscomplex(jo) not implemented"))
+
+# isinvertible(jo)
+isinvertible(A::joAbstractOperator) = throw(joAbstractOperatorException("isinvertible(jo) not implemented"))
+
+# islinear(jo)
+islinear(A::joAbstractOperator,v::Bool=false) = throw(joAbstractOperatorException("islinear(jo) not implemented"))
+
+# isadjoint(jo)
+isadjoint(A::joAbstractOperator,ctmult::Number=1,v::Bool=false) = throw(joAbstractOperatorException("isadjoint(jo) not implemented"))
