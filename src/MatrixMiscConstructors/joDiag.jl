0509e246e94ee7c43604bf4c69d874e4883741c2
Henryk Modzelewski <henryk_modzelewski@mac.com>
2017-01-13 12:22:53 -0800

    optimized joDiag

--- src/MatrixMiscConstructors/joDiag.jl
+++ src/MatrixMiscConstructors/joDiag.jl
@@ -3,10 +3,10 @@
 export joDiag
 joDiag{ODT}(v::AbstractVector{ODT}) =
     joMatrix{ODT}("joDiag",length(v),length(v),
-        v1->spdiagm(v)*v1,
-        v2->spdiagm(v)*v2,
-        v3->spdiagm(conj(v))*v3,
-        v4->spdiagm(conj(v))*v4,
+        v1->Diagonal(v)*v1,
+        v2->Diagonal(v)*v2,
+        v3->Diagonal(conj(v))*v3,
+        v4->Diagonal(conj(v))*v4,
         @NF, @NF, @NF, @NF
         )
 
