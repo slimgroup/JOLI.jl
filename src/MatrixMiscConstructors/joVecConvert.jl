58e3639ef0efa63192aaa806207878983e875e7c
Henryk Modzelewski <henryk_modzelewski@mac.com>
2017-02-03 18:57:44 -0800

    ...

--- src/MatrixMiscConstructors/joVecConvert.jl
+++ src/MatrixMiscConstructors/joVecConvert.jl
@@ -4,17 +4,17 @@ export joReal, joImag, joConj
 joReal(m::Integer=1,ODT::DataType=Int8) =
     joMatrix{ODT}("joReal",m,m,
         v1->real(v1),
-        v2->real(v2),
-        v3->real(v3),
+        v2->complex(v2),
+        v3->complex(v3),
         v4->real(v4),
         @NF, @NF, @NF, @NF
         )
 joImag(m::Integer=1,ODT::DataType=Int8) =
     joMatrix{ODT}("joImag",m,m,
         v1->imag(v1),
-        v2->imag(v2),
-        v3->imag(v3),
-        v4->imag(v4),
+        v2->complex(0,v2),
+        v3->complex(0,v3),
+        v4->complex(0,-v4),
         @NF, @NF, @NF, @NF
         )
 joConj(m::Integer=1,ODT::DataType=Int8) =
