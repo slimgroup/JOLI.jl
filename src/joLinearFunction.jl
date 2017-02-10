07e6ed8f58be0f23e23d5d5f84825742ad088879
Henryk Modzelewski <henryk_modzelewski@mac.com>
2017-02-09 17:19:49 -0800

    fixed dimensions for joOnes in +(jo,num)

--- src/joLinearFunction.jl
+++ src/joLinearFunction.jl
@@ -203,8 +203,8 @@ function +{AODT,bDT<:Number}(A::joLinearFunction{AODT},b::bDT)
     nODT=promote_type(AODT,bDT)
     return joLinearFunction{nODT}("("*A.name*"+N)",A.m,A.n,
         v1->A.fop(v1)+b*joOnes(A.m,A.n)*v1,
-        v2->A.fop_T(v2)+b*joOnes(A.m,A.n)*v2,
-        v3->A.fop_CT(v3)+conj(b)*joOnes(A.m,A.n)*v3,
+        v2->A.fop_T(v2)+b*joOnes(A.n,A.m)*v2,
+        v3->A.fop_CT(v3)+conj(b)*joOnes(A.n,A.m)*v3,
         v4->A.fop_C(v4)+conj(b)*joOnes(A.m,A.n)*v4,
         @NF, @NF, @NF, @NF
         )
--- src/joLinearOperator.jl
+++ src/joLinearOperator.jl
@@ -310,8 +310,8 @@ function +{AODT,bDT<:Number}(A::joLinearOperator{AODT},b::bDT)
     nODT=promote_type(AODT,bDT)
     return joLinearOperator{nODT}("("*A.name*"+N)",A.m,A.n,
         v1->A.fop(v1)+b*joOnes(A.m,A.n)*v1,
-        v2->A.fop_T(v2)+b*joOnes(A.m,A.n)*v2,
-        v3->A.fop_CT(v3)+conj(b)*joOnes(A.m,A.n)*v3,
+        v2->A.fop_T(v2)+b*joOnes(A.n,A.m)*v2,
+        v3->A.fop_CT(v3)+conj(b)*joOnes(A.n,A.m)*v3,
         v4->A.fop_C(v4)+conj(b)*joOnes(A.m,A.n)*v4,
         @NF, @NF, @NF, @NF
         )
@@ -320,8 +320,8 @@ function +{AODT,bDT<:Number}(A::joAbstractLinearOperator{AODT},b::bDT)
     nODT=promote_type(AODT,bDT)
     return joLinearOperator{nODT}("("*A.name*"+N)",size(A,1),size(A,2),
         v1->A*v1+b*joOnes(A.m,A.n)*v1,
-        v2->A.'*v2+b*joOnes(A.m,A.n)*v2,
-        v3->A'*v3+conj(b)*joOnes(A.m,A.n)*v3,
+        v2->A.'*v2+b*joOnes(A.n,A.m)*v2,
+        v3->A'*v3+conj(b)*joOnes(A.n,A.m)*v3,
         v4->conj(A)*v4+conj(b)*joOnes(A.m,A.n)*v4,
         @NF, @NF, @NF, @NF
         )
