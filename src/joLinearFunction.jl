5f3673a7c96f2f4b1d08107e1a766d9c41f7c1f6
Henryk Modzelewski <henryk_modzelewski@mac.com>
2017-02-09 17:45:32 -0800

    fixed nullables in +(jo,num)

--- src/joLinearFunction.jl
+++ src/joLinearFunction.jl
@@ -203,9 +203,9 @@ function +{AODT,bDT<:Number}(A::joLinearFunction{AODT},b::bDT)
     nODT=promote_type(AODT,bDT)
     return joLinearFunction{nODT}("("*A.name*"+N)",A.m,A.n,
         v1->A.fop(v1)+b*joOnes(A.m,A.n)*v1,
-        v2->A.fop_T(v2)+b*joOnes(A.n,A.m)*v2,
-        v3->A.fop_CT(v3)+conj(b)*joOnes(A.n,A.m)*v3,
-        v4->A.fop_C(v4)+conj(b)*joOnes(A.m,A.n)*v4,
+        v2->get(A.fop_T)(v2)+b*joOnes(A.n,A.m)*v2,
+        v3->get(A.fop_CT)(v3)+conj(b)*joOnes(A.n,A.m)*v3,
+        v4->get(A.fop_C)(v4)+conj(b)*joOnes(A.m,A.n)*v4,
         @NF, @NF, @NF, @NF
         )
 end
