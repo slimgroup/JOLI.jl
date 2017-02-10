1a155dcea80c500b7bf425e0834eeb6831bd2bca
Henryk Modzelewski <henryk_modzelewski@mac.com>
2017-02-10 11:46:42 -0800

    added explicit range to joMatrix

--- src/joMatrix.jl
+++ src/joMatrix.jl
@@ -63,15 +63,17 @@ joMatrix outer constructor
 
 # Example
 
-- joMatrix(rand(4,3))
+- joMatrix(rand(4,3)) # implicit domain and range
 
-- joMatrix(rand(4,3),Float32)
+- joMatrix(rand(4,3),Float32) # implicit range
 
-- joMatrix(rand(4,3);name="my matrix")
+- joMatrix(rand(4,3),Float32,Float64)
+
+- joMatrix(rand(4,3);name="my matrix") # adding name
 
 """
-joMatrix{EDT}(array::AbstractMatrix{EDT},DDT::DataType=EDT;name::String="joMatrix") = # fix,DDT,RDT
-    joMatrix{EDT,DDT,promote_type(EDT,DDT)}(name,size(array,1),size(array,2),
+joMatrix{EDT}(array::AbstractMatrix{EDT},DDT::DataType=EDT,RDT::DataType=promote_type(EDT,DDT);name::String="joMatrix") =
+    joMatrix{EDT,DDT,RDT}(name,size(array,1),size(array,2),
         v1->array*v1,
         v2->array.'*v2,
         v3->array'*v3,
