aeb5428955e786755dbb31fc62abb631468c6329
Henryk Modzelewski <henryk_modzelewski@mac.com>
2017-03-09 18:42:13 -0800

    allow deep copy of input matrix

--- src/joMatrix/constructors.jl
+++ src/joMatrix/constructors.jl
@@ -4,28 +4,30 @@
 """
 joMatrix outer constructor
 
-    joMatrix(array::AbstractMatrix;DDT::DataType=EDT,RDT::DataType=promote_type(EDT,DDT),name::String="joMatrix")
+    joMatrix(array::AbstractMatrix;DDT::DataType=EDT,RDT::DataType=promote_type(EDT,DDT),name::String="joMatrix",getcopy::Bool=false)
 
 # Example
 - joMatrix(rand(4,3)) # implicit domain and range
 - joMatrix(rand(4,3);DDT=Float32) # implicit range
 - joMatrix(rand(4,3);DDT=Float32,RDT=Float64)
 - joMatrix(rand(4,3);name="my matrix") # adding name
+- joMatrix(rand(4,3);getcopy=false) # do not make deep copy of input array
 
 # Notes
 - if DDT:<Real for complex matrix then imaginary part will be neglected for transpose/conjugated-transpose operator
 - if RDT:<Real for complex matrix then imaginary part will be neglected for forward/conjugate operator
 
 """
-joMatrix{EDT}(array::AbstractMatrix{EDT};DDT::DataType=EDT,RDT::DataType=promote_type(EDT,DDT),name::String="joMatrix") =
-    joMatrix{EDT,DDT,RDT}(name,size(array,1),size(array,2),
-        v1->jo_convert(RDT,array*v1,false),
-        v2->jo_convert(DDT,array.'*v2,false),
-        v3->jo_convert(DDT,array'*v3,false),
-        v4->jo_convert(RDT,conj(array)*v4,false),
-        v5->jo_convert(DDT,array\v5,false),
-        v6->jo_convert(RDT,array.'\v6,false),
-        v7->jo_convert(RDT,array'\v7,false),
-        v8->jo_convert(DDT,conj(array)\v8,false)
+function joMatrix{EDT}(array::AbstractMatrix{EDT};DDT::DataType=EDT,RDT::DataType=promote_type(EDT,DDT),name::String="joMatrix",getcopy::Bool=true)
+    Array= getcopy ? Base.deepcopy(array) : array
+    joMatrix{EDT,DDT,RDT}(name,size(Array,1),size(Array,2),
+        v1->jo_convert(RDT,Array*v1,false),
+        v2->jo_convert(DDT,Array.'*v2,false),
+        v3->jo_convert(DDT,Array'*v3,false),
+        v4->jo_convert(RDT,conj(Array)*v4,false),
+        v5->jo_convert(DDT,Array\v5,false),
+        v6->jo_convert(RDT,Array.'\v6,false),
+        v7->jo_convert(RDT,Array'\v7,false),
+        v8->jo_convert(DDT,conj(Array)\v8,false)
         )
-
+end
