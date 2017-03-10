78428a992e4bec3515476d9b10bead5b2591ca71
Henryk Modzelewski <henryk_modzelewski@mac.com>
2017-03-10 14:13:39 -0800

    copy option

--- src/joMatrix/constructors.jl
+++ src/joMatrix/constructors.jl
@@ -4,30 +4,31 @@
 """
 joMatrix outer constructor
 
-    joMatrix(array::AbstractMatrix;DDT::DataType=EDT,RDT::DataType=promote_type(EDT,DDT),name::String="joMatrix",getcopy::Bool=false)
+    joMatrix(m::AbstractMatrix;DDT::DataType=EDT,RDT::DataType=promote_type(EDT,DDT),name::String="joMatrix",makecopy::Bool=false)
 
 # Example
 - joMatrix(rand(4,3)) # implicit domain and range
 - joMatrix(rand(4,3);DDT=Float32) # implicit range
 - joMatrix(rand(4,3);DDT=Float32,RDT=Float64)
 - joMatrix(rand(4,3);name="my matrix") # adding name
-- joMatrix(rand(4,3);getcopy=false) # do not make deep copy of input array
+- joMatrix(rand(4,3);makecopy=true) # make deep copy of input m
 
 # Notes
 - if DDT:<Real for complex matrix then imaginary part will be neglected for transpose/conjugated-transpose operator
 - if RDT:<Real for complex matrix then imaginary part will be neglected for forward/conjugate operator
 
 """
-function joMatrix{EDT}(array::AbstractMatrix{EDT};DDT::DataType=EDT,RDT::DataType=promote_type(EDT,DDT),name::String="joMatrix",getcopy::Bool=true)
-    Array= getcopy ? Base.deepcopy(array) : array
-    joMatrix{EDT,DDT,RDT}(name,size(Array,1),size(Array,2),
-        v1->jo_convert(RDT,Array*v1,false),
-        v2->jo_convert(DDT,Array.'*v2,false),
-        v3->jo_convert(DDT,Array'*v3,false),
-        v4->jo_convert(RDT,conj(Array)*v4,false),
-        v5->jo_convert(DDT,Array\v5,false),
-        v6->jo_convert(RDT,Array.'\v6,false),
-        v7->jo_convert(RDT,Array'\v7,false),
-        v8->jo_convert(DDT,conj(Array)\v8,false)
+function joMatrix{EDT}(m::AbstractMatrix{EDT};DDT::DataType=EDT,RDT::DataType=promote_type(EDT,DDT),name::String="joMatrix",makecopy::Bool=false)
+    mc= makecopy ? Base.deepcopy(m) : m
+    return joMatrix{EDT,DDT,RDT}(name,size(mc,1),size(mc,2),
+        v1->jo_convert(RDT,mc*v1,false),
+        v2->jo_convert(DDT,mc.'*v2,false),
+        v3->jo_convert(DDT,mc'*v3,false),
+        v4->jo_convert(RDT,conj(mc)*v4,false),
+        v5->jo_convert(DDT,mc\v5,false),
+        v6->jo_convert(RDT,mc.'\v6,false),
+        v7->jo_convert(RDT,mc'\v7,false),
+        v8->jo_convert(DDT,conj(mc)\v8,false)
         )
 end
+
--- src/joMatrixConstructors/joDiag.jl
+++ src/joMatrixConstructors/joDiag.jl
@@ -1,12 +1,14 @@
 # diagonal operators: joDiag
 
 export joDiag
-joDiag{EDT}(v::AbstractVector{EDT};DDT::DataType=EDT,RDT::DataType=promote_type(EDT,DDT)) =
-    joMatrix{EDT,DDT,RDT}("joDiag",length(v),length(v),
-        v1->jo_convert(RDT,Diagonal(v)*v1,false),
-        v2->jo_convert(DDT,Diagonal(v)*v2,false),
-        v3->jo_convert(DDT,Diagonal(conj(v))*v3,false),
-        v4->jo_convert(RDT,Diagonal(conj(v))*v4,false),
+function joDiag{EDT}(v::AbstractVector{EDT};DDT::DataType=EDT,RDT::DataType=promote_type(EDT,DDT),makecopy::Bool=false)
+    vc= makecopy ? Base.deepcopy(v) : v
+    return joMatrix{EDT,DDT,RDT}("joDiag",length(vc),length(vc),
+        v1->jo_convert(RDT,Diagonal(vc)*v1,false),
+        v2->jo_convert(DDT,Diagonal(vc)*v2,false),
+        v3->jo_convert(DDT,Diagonal(conj(vc))*v3,false),
+        v4->jo_convert(RDT,Diagonal(conj(vc))*v4,false),
         @joNF, @joNF, @joNF, @joNF
         )
+end
 
