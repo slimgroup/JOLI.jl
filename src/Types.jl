98884afd61b0cc55f18d77d3218613f50be40cdd
Henryk Modzelewski <henryk_modzelewski@mac.com>
2017-03-28 15:23:50 -0700

    joNumber type/constructor

---
+++ src/Types.jl
@@ -0,0 +1,24 @@
+############################################################
+# Misc Types ###############################################
+############################################################
+
+export joNumber
+"""
+joNumber type
+
+A number type to use for jo operations with number
+
+# TYPE PARAMETERS
+- DDT::DataType : domain DataType
+- RDT::DataType : range DataType
+
+# FIELDS
+- ddt::DDT : number to use when acting on vector to return domain vector
+- rdt::RDT : number to use when acting on vector to return range vector
+
+"""
+immutable joNumber{DDT<:Number,RDT<:Number}
+    ddt::DDT
+    rdt::RDT
+end
+
--- src/joLinearOperator/constructors.jl
+++ src/joLinearOperator/constructors.jl
@@ -1,3 +1,23 @@
 ############################################################
 ## joLinearOperator - outer constructors
 
+"""
+joNumber outer constructor
+
+    joNumber(num,A::joAbstractLinearOperator{DDT,RDT})
+
+Create joNumber with types matching the given operator.
+
+"""
+joNumber{NT<:Number,DDT,RDT}(num::NT,A::joAbstractLinearOperator{DDT,RDT}) =
+    joNumber{DDT,RDT}(jo_convert(DDT,num),jo_convert(RDT,num))
+"""
+joNumber outer constructor
+
+    joNumber(num)
+
+Create joNumber with types matching given number
+
+"""
+joNumber{NT<:Number}(num::NT) = joNumber{NT,NT}(num,num)
+
