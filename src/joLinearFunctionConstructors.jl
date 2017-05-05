24ef723b8221ce43e1e59f18383b43b31bec1a6a
Henryk Modzelewski <henryk_modzelewski@mac.com>
2017-05-05 12:33:14 -0700

    fixed typo

--- src/joLinearFunctionConstructors.jl
+++ src/joLinearFunctionConstructors.jl
@@ -8,5 +8,5 @@ include("joLinearFunctionConstructors/joDFT.jl")
 # DCT operators: joDCT
 include("joLinearFunctionConstructors/joDCT.jl")
 
-# FFT operators: joCurvelet2D
+# Curvelab operators: joCurvelet2D
 include("joLinearFunctionConstructors/joCurevelet2d.jl")
