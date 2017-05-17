ed745b9f13665c0788826807ba2be32866fca93a
Henryk Modzelewski <henryk_modzelewski@mac.com>
2017-05-16 17:09:35 -0700

    updated docs

--- src/joMatrixConstructors/joSincInterp.jl
+++ src/joMatrixConstructors/joSincInterp.jl
@@ -1,16 +1,18 @@
 export joSincInterp
 
-# joSincInterp
-#
-# sinc interpolation matrix for interpolating functions f defined on grid xin to functions defined on grid xout
-#
-# Note: if xout has more than one point, the spacings of xin and xout are normalized to the spacing of xout
-# 
-# Parameters:
-#   xin  - 1D input grid
-#   xout - 1D output grid
-#   r    - kaiser window parameter (default: 0, no windowing)
-#
+"""
+ joSincInterp
+
+ sinc interpolation matrix for interpolating functions f defined on grid xin to functions defined on grid xout
+
+ Note: if xout has more than one point, the spacings of xin and xout are normalized to the spacing of xout
+ 
+ Parameters:
+   xin  - 1D input grid
+   xout - 1D output grid
+   r    - kaiser window parameter (default: 0, no windowing)
+
+"""
 function joSincInterp{T<:AbstractFloat,I<:Integer}(xin::AbstractArray{T,1},xout::AbstractArray{T,1};r::I=0)
     if length(xout)>1
         dx = xout[2]-xout[1]
