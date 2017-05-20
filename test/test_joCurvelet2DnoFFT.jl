72596aee367c1c848362852591bd22ae1dd10156
SoftAdmin <softadmin@slim-mail.eos.ubc.ca>
2017-05-19 17:15:28 -0700

    added joCurvelet2DnoFFT

--- test/test_joCurvelet2DnoFFT.jl
+++ test/test_joCurvelet2DnoFFT.jl
@@ -11,15 +11,21 @@ tsname="joCurvelet2DnoFFT"
 @testset "$tsname" begin
 for t=1:T # start test loop
     m=32*t
-    Awr=joCurvelet2DnoFFT(m,m)
-    Acr=joCurvelet2DnoFFT(m,m;all_crvlts=true)
+    Awr=joCurvelet2DnoFFT(m,m)*joDFT(m,m)
+    Awc=joCurvelet2DnoFFT(m,m;real_crvlts=false)*joDFT(m,m)
+    Acr=joCurvelet2DnoFFT(m,m;all_crvlts=true)*joDFT(m,m)
+    Acc=joCurvelet2DnoFFT(m,m;all_crvlts=true,real_crvlts=false)*joDFT(m,m)
 
     println("$tsname ($m,$m)")
     @testset "$m x $m" begin
         @test isadjoint(Awr)[1]
+        @test isadjoint(Awc)[1]
         @test isadjoint(Acr)[1]
+        @test isadjoint(Acc)[1]
         @test islinear(Awr)[1]
+        @test islinear(Awc)[1]
         @test islinear(Acr)[1]
+        @test islinear(Acc)[1]
     end
     
 end # end test loop
