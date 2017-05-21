4f6cdeb8ab93b5fe4300a839e9d089aa3f6f3503
Henryk Modzelewski <henryk_modzelewski@mac.com>
2017-05-21 13:54:33 -0700

    fixed adjoint test for real oCurvelet2DnoFFT

--- test/test_joCurvelet2DnoFFT.jl
+++ test/test_joCurvelet2DnoFFT.jl
@@ -11,16 +11,20 @@ tsname="joCurvelet2DnoFFT"
 @testset "$tsname" begin
 for t=1:T # start test loop
     m=32*t
-    Awr=joCurvelet2DnoFFT(m,m)*joDFT(m,m)
-    Awc=joCurvelet2DnoFFT(m,m;real_crvlts=false)*joDFT(m,m)
-    Acr=joCurvelet2DnoFFT(m,m;all_crvlts=true)*joDFT(m,m)
-    Acc=joCurvelet2DnoFFT(m,m;all_crvlts=true,real_crvlts=false)*joDFT(m,m)
+    Awr=joCurvelet2DnoFFT(m,m)
+    AwR=joCurvelet2DnoFFT(m,m)*joDFT(m,m)
+    Awc=joCurvelet2DnoFFT(m,m;real_crvlts=false)
+    Acr=joCurvelet2DnoFFT(m,m;all_crvlts=true)
+    AcR=joCurvelet2DnoFFT(m,m;all_crvlts=true)*joDFT(m,m)
+    Acc=joCurvelet2DnoFFT(m,m;all_crvlts=true,real_crvlts=false)
 
     println("$tsname ($m,$m)")
     @testset "$m x $m" begin
-        @test isadjoint(Awr)[1]
+        @test isadjoint(Awr;userange=true)[1]
+        @test isadjoint(AwR)[1]
         @test isadjoint(Awc)[1]
-        @test isadjoint(Acr)[1]
+        @test isadjoint(Acr;userange=true)[1]
+        @test isadjoint(AcR)[1]
         @test isadjoint(Acc)[1]
         @test islinear(Awr)[1]
         @test islinear(Awc)[1]
