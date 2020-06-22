56f68c0bf152fed10df2341271d3e7ac958e7aea
mloubout <mathias.louboutin@gmail.com>
2020-06-22 09:17:32 -0400

    add CI + ortho test

--- test/test_joSWT.jl
+++ test/test_joSWT.jl
@@ -1,12 +1,14 @@
+pywavelet= false
 try
     global pywt = pyimport("pywt")
     global swt = pywt.swt
+    pywavelet = true
 catch
     @warn "Skipping joSWTtests - PyCall clouldn't import PyWavelets"
 end
 
 families = ("haar", "db", "sym", "coif")
-wavelets = vcat([pywt.wavelist(name) for name in families]...)
+pywavelet ? wavelets = vcat([pywt.wavelist(name) for name in families]...) : wavelets = []
 
 tsname="joSWT"
 @testset "$tsname" begin
@@ -14,11 +16,14 @@ for w in wavelets
     m = 1024
     Swt = joSWT(m, w)
     u = rand(m)
-    v = rand(size(Swt, 1))
+    v = Swt*u
     verbose && println("$tsname, $w")
     @testset "$w" begin
         @test isadjoint(Swt)[1]
         @test islinear(Swt)[1]
+        # Test orthogonality
+        @test norm((adjoint(Swt)*Swt)* u - u) < joTol
+        @test norm((Swt*adjoint(Swt))* v - v) < joTol
     end
     
 end # end test loop
