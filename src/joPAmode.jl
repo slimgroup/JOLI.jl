f552b5f8cccb4ff01ceec8f98b35e5fe69e9fdff
Henryk Modzelewski <henryk_modzelewski@mac.com>
2020-07-30 20:23:01 -0700

    ...

--- src/joPAmode.jl
+++ src/joPAmode.jl
@@ -16,21 +16,22 @@ export jo_PAmode
 """
 jo_PAmode - set and query parallel mode
 
-jo_PAmode() - query parallel mode
+jo_PAmode(verbose=false) - query parallel mode
 
-jo_PAmode( ":SA | :DA" ) - set parallel mode to respectively shared or distributed arrays
+jo_PAmode([ :SA | :DA ]; verbose=false) - set parallel mode to respectively shared or distributed arrays
 
 """
-function jo_PAmode()
+function jo_PAmode(;verbose::Bool=false)
     if PAmode==:SA
-        println("Parallel mode is set to :$PAmode (shared arrays)")
+        verbose && println("Parallel mode is set to :$PAmode (shared arrays)")
     elseif PAmode==:DA
-        println("Parallel mode is set to :$PAmode (distributed arrays)")
+        verbose && println("Parallel mode is set to :$PAmode (distributed arrays)")
     else
         error("Unknown parallel mode $m")
     end
+    return PAmode
 end
-function jo_PAmode(m::Symbol)
+function jo_PAmode(m::Symbol;verbose::Bool=false)
     if m==:SA
         global joPAdistribute       = joSAdistribute
         global joPAdistributedLinOp = joSAdistributedLinOp
@@ -41,7 +42,7 @@ function jo_PAmode(m::Symbol)
         global prand    = srand
         global prandn   = srandn
         global PAmode = :SA
-        println("Parallel mode is set to :$PAmode (shared arrays)")
+        verbose && println("Parallel mode is set to :$PAmode (shared arrays)")
     elseif m==:DA
         global joPAdistribute       = joDAdistribute
         global joPAdistributedLinOp = joDAdistributedLinOp
@@ -52,9 +53,9 @@ function jo_PAmode(m::Symbol)
         global prand    = drand
         global prandn   = drandn
         global PAmode = :DA
-        println("Parallel mode is set to :$PAmode (distributed arrays)")
+        verbose && println("Parallel mode is set to :$PAmode (distributed arrays)")
     else
         error("Unknown parallel mode $m")
     end
-    nothing
+    return nothing
 end
