6132b0740035dfd46c2c80844037f5ee748a4619
Francis <ziyi.yin@gatech.edu>
2021-11-15 12:11:08 -0500

    restructure the JRM test to see if it works as JRM expects

--- test/test_joJRM.jl
+++ test/test_joJRM.jl
@@ -1,6 +1,6 @@
 T=3
 tsname="joJRM"
-rtol = 1f-4
+rtol = 1f-5
 @testset "$tsname" begin
     for t=2:T # start test loop
     
@@ -10,17 +10,25 @@ rtol = 1f-4
 
         verbose && println("$tsname ($m)")
         @testset "$tsname w/ $t vintages" begin
-            # test adjoint
-            x = [randn(Float32, t) for i = 1:t+1]
-            y = [randn(Float64, t) for i = 1:t]
-            d1 = dot(y,A*x)
-            d2 = dot(x,A'*y)
-            @test isapprox(d1, d2; rtol=rtol)
-            # test linearity
             x1 = [randn(Float32, t) for i = 1:t+1]
             x2 = [randn(Float32, t) for i = 1:t+1]
+            y = [randn(Float64, t) for i = 1:t]
+            # test if it works as expected
+            Ax1 = A*x1
+            Ax1_ = ops.*[1f0/γ*x1[1]+x1[i] for i = 2:t+1]
+            @test isapprox(Ax1, Ax1_; rtol=rtol)
+            Aadjy = A'*y
+            Aadjy_ = [ops[i]'*y[i] for i = 1:t]
+            Aadjy_ = [1f0/γ*sum(Aadjy_), Aadjy_...]
+            @test isapprox(Aadjy, Aadjy_; rtol=rtol)
+            # test adjoint
+            d1 = dot(y,Ax1)
+            d2 = dot(x1,Aadjy)
+            @test isapprox(d1, d2; rtol=rtol)
+            # test linearity
+            Ax2 = A*x2
             y1 = A*(x1+x2)
-            y2 = A*x1 + A*x2
+            y2 = Ax1+Ax2
             @test isapprox(y1, y2; rtol=rtol)
         end
     
