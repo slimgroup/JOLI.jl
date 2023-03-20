2afe35dce5f56ab78eef317981d058629231f91a
mloubout <mathias.louboutin@gmail.com>
2023-03-20 08:18:57 -0400

    Fix #43

--- src/rrule.jl
+++ src/rrule.jl
@@ -1,7 +1,7 @@
 function rrule(::typeof(*), A::joAbstractLinearOperator{ADDT,ARDT}, v) where {ADDT,ARDT}
   Y = A*v
   function time_pullback(dy)
-    DY = unthunk(Vector(dy))
+    DY = collect(unthunk(dy))
     return NoTangent(), NoTangent(), @thunk(A' * DY)
   end
   return Y, time_pullback
